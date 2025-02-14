3a9c0ab5c94422f1913112a5c387b6068568ab72
David McGinnis <david.mcginnis@bl-mc.se>
2025-02-14 11:14:24 +0100

    working with picoW

--- blinky-cube-code/blinky-cube-code.ino
+++ blinky-cube-code/blinky-cube-code.ino
@@ -1,121 +1,120 @@
-boolean printDiagnostics = false;
+#define BLINKY_DIAG        0
+#define CUBE_DIAG          0
+#define COMM_LED_PIN       2
+#define RST_BUTTON_PIN     3
+#define COND_MULT         10.0
+#include <BlinkyPicoW.h>
 
-union CubeData
+struct CubeSetting
 {
-  struct
-  {
-    int16_t state;          
-    int16_t watchdog;       
-    int16_t chipTemp; 
-    int16_t sensor1;       
-    int16_t sensor2;        
-    int16_t sensor3;        
-  };
-  byte buffer[12];
+  uint16_t publishInterval;
+  uint16_t nsamples;
 };
-CubeData cubeData;
+CubeSetting setting;
 
-#include "BlinkyPicoWCube.h"
-
-
-int commLEDPin = 2;
-int commLEDBright = 255; 
-int resetButtonPin = 3;
+struct CubeReading
+{
+  uint16_t sensor1;
+  uint16_t sensor2;
+  uint16_t refVolts;
+  uint16_t bandWidth;
+};
+CubeReading reading;
 
 unsigned long lastPublishTime;
-unsigned long publishInterval = 2000;
+
 
 float fsensor1;
 float fsensor2;
 float fsensor3;
-float fsamples = 2000;
+int digCount;
 
-void setupServerComm()
+void setupBlinky()
 {
-  // Optional setup to overide defaults
-  if (printDiagnostics) Serial.begin(115200);
-  BlinkyPicoWCube.setChattyCathy(printDiagnostics);
-  BlinkyPicoWCube.setWifiTimeoutMs(20000);
-  BlinkyPicoWCube.setWifiRetryMs(20000);
-  BlinkyPicoWCube.setMqttRetryMs(3000);
-  BlinkyPicoWCube.setResetTimeoutMs(10000);
-  BlinkyPicoWCube.setHdwrWatchdogMs(8000);
-  BlinkyPicoWCube.setBlMqttKeepAlive(8);
-  BlinkyPicoWCube.setBlMqttSocketTimeout(4);
-  BlinkyPicoWCube.setMqttLedFlashMs(10);
-  BlinkyPicoWCube.setWirelesBlinkMs(100);
-  BlinkyPicoWCube.setMaxNoMqttErrors(5);
-  
-  // Must be included
-  BlinkyPicoWCube.init(commLEDPin, commLEDBright, resetButtonPin);
+  if (BLINKY_DIAG > 0) Serial.begin(9600);
+
+  BlinkyPicoW.setMqttKeepAlive(15);
+  BlinkyPicoW.setMqttSocketTimeout(4);
+  BlinkyPicoW.setMqttPort(1883);
+  BlinkyPicoW.setMqttLedFlashMs(100);
+  BlinkyPicoW.setHdwrWatchdogMs(8000);
+
+  BlinkyPicoW.begin(BLINKY_DIAG, COMM_LED_PIN, RST_BUTTON_PIN, true, sizeof(setting), sizeof(reading));
 }
 
 void setupCube()
 {
+  if (CUBE_DIAG > 0) Serial.begin(9600);
+  setting.publishInterval = 4000;
   lastPublishTime = millis();
-  cubeData.state = 1;
-  cubeData.watchdog = 0;
+  setting.nsamples = 2000;
+
   analogReadResolution(12);
   analogRead(A0);
   analogRead(A1);
   analogRead(A2);
-  delay(1000);
+  delay(100);
   fsensor1 = (float) analogRead(A0);
   fsensor2 = (float) analogRead(A1);
   fsensor3 = (float) analogRead(A2);
+  digCount = 1;
 
 }
 
-void cubeLoop()
+void loopCube()
 {
-  unsigned long nowTime = millis();
+  unsigned long now = millis();
 
-  fsensor1 = fsensor1 + ((float) analogRead(A0) - fsensor1) / fsamples;
-  fsensor2 = fsensor2 + ((float) analogRead(A1) - fsensor2) / fsamples;
-  fsensor3 = fsensor3 + ((float) analogRead(A2) - fsensor3) / fsamples;
+  fsensor1 = fsensor1 + ((float) analogRead(A0) - fsensor1) / ((float) setting.nsamples);
+  fsensor2 = fsensor2 + ((float) analogRead(A1) - fsensor2) / ((float) setting.nsamples);
+  fsensor3 = fsensor3 + ((float) analogRead(A2) - fsensor3) / ((float) setting.nsamples);
+ ++digCount;
   
   
-  if ((nowTime - lastPublishTime) > publishInterval)
+  if ((now - lastPublishTime) > setting.publishInterval)
   {
+    float fbandwidth = 500.0 * ( ((float) digCount) / ((float) setting.publishInterval) ) / ((float) setting.nsamples);
+    reading.bandWidth = (uint16_t) fbandwidth;   
+    digCount = 0;
+    
     readSensor();
-    lastPublishTime = nowTime;
-    cubeData.watchdog = cubeData.watchdog + 1;
-    if (cubeData.watchdog > 32760) cubeData.watchdog= 0 ;
-    BlinkyPicoWCube.publishToServer();
-
+    lastPublishTime = now;
+    boolean successful = BlinkyPicoW.publishCubeData((uint8_t*) &setting, (uint8_t*) &reading, false);
   }  
+  boolean newSettings = BlinkyPicoW.retrieveCubeSetting((uint8_t*) &setting);
+  if (newSettings)
+  {
+    if (setting.publishInterval < 1000) setting.publishInterval = 1000;
+    if (setting.nsamples < 1) setting.nsamples = 1;
+    fsensor1 = (float) analogRead(A0);
+    fsensor2 = (float) analogRead(A1);
+    fsensor3 = (float) analogRead(A2);
+  }
   
 }
 void readSensor()
 {
-  cubeData.chipTemp = (int16_t) (analogReadTemp() * 100.0);
-  cubeData.sensor1 = (int16_t) (fsensor1 * 8.0);
-  cubeData.sensor2 = (int16_t) (fsensor2 * 8.0);
-  cubeData.sensor3 = (int16_t) (fsensor3 * 8.0);
+  
+  reading.sensor1 = conductance(fsensor1, fsensor3);
+  reading.sensor2 = conductance(fsensor2, fsensor3);
+  reading.refVolts = (uint16_t) (3300 * fsensor3 / 4096.0);
 
-  if (printDiagnostics)
+  if (CUBE_DIAG > 0)
   {
-/*
-    Serial.print(cubeData.chipTemp);
+    Serial.print(reading.sensor1);
     Serial.print(",");
-    Serial.print(cubeData.sensor1);
+    Serial.print(reading.sensor2);
     Serial.print(",");
-    Serial.print(cubeData.sensor2);
-    Serial.print(",");
-    Serial.println(cubeData.sensor3);
-*/
+    Serial.println(reading.refVolts);
   }
 
 }
-
-
-void handleNewSettingFromServer(uint8_t address)
+uint16_t conductance(float sensorVal, float openSensorVal)
 {
-  switch(address)
-  {
-    case 0:
-      break;
-    default:
-      break;
-  }
+    float traw = sensorVal / 4096.0;
+    float topen = openSensorVal / 4096.0;
+    float fcond = 10.0 * COND_MULT * (topen * (1.0 - traw) - traw * (1.0 - topen)) / (topen * traw);
+    if (fcond < 0.1) fcond = 0.1;
+    if (fcond > 65535) fcond = 65535.0;
+    return (uint16_t) fcond;
 }
